bfe02c11fb5c71f49012855d4603b738c2e74203
Tamas <30211285@MB319-STU5275>
2018-10-24 16:18:02 +0100

    Calculation page

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,43 @@
-let btnNext = document.getElementById("btnBinAddNext");
+//Binary Addition Step Counter
+let binAddStep = 0;
+//Binary Section shown
+let binShown = false;
+let binAddShown = false;
 
-let btnPrev = document.getElementById("btnBinAddPrevious");
+let binary = document.getElementById("Binary");//Binary section
+let btnBinary = document.getElementById("btnBinary");
+btnBinary.addEventListener("click", function(){
+    Display(binary,binShown);
+});
+
+let binAdd = document.getElementById("binAdd");
+let btnBinAdd = document.getElementById("btnBinAdd");
+btnBinAdd.addEventListener("click", function(){
+    Display(binAdd,binAddShown);
+});
+
+//Toggle display of Binary section
+function Display(sect,sectShown){
+    if(sectShown == false){
+        sect.removeAttribute("class", "hide");
+        sectShown = true;
+
+        if(sect == binary){
+            binShown = sectShown;
+        }
+        else if(sect == binAdd){
+            binAdd = sectShown;
+        }
+
+    }
+    else{
+        sect.setAttribute("class", "hide");
+        sectShown = false;
+        if(sect == binary){
+            binShown = sectShown;
+        }
+        else if(sect == binAdd){
+            binAdd = sectShown;
+        }
+    }
+};
